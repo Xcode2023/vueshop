4739f198118cf8e907cbee7b1240f2b5d2230949
Xcode2023 <1806120396@qq.com>
2023-09-10 15:59:41 +0800

    hello

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -7,11 +7,32 @@ import {ref,onMounted} from 'vue'
 // import module from 'P/spline/rabbit.spline'
 import { Application } from "@splinetool/runtime";
 
+import gsap from 'gsap'
+
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+const animate1=()=>{
+gsap.from(canvas.value,{
+  y:-30,
+  opacity:0,
+  rotation:30
+})
+
+  // ScrollTrigger.create(
+  //   {
+
+  //   }
+  // )
+}
+
 const canvas = ref(null)
 onMounted(()=> {
   const app = new Application(canvas.value);
   app.load("public/spline/rabbit.spline");
+  setTimeout(() => {
+      animate1()
 
+  }, 3000);
 })
 
 
